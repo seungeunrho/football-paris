80362c61866250b957285d9f6f66b75fc6fea086
Seongho Son <geronest@gmail.com>
2020-11-16 01:05:11 +0000

    1. actor_self dumps last len_rollout transitions in logs/@@@/dumps/ 2. see_dump.py: opens dump files, can look around

--- actor.py
+++ actor.py
@@ -9,6 +9,7 @@ import torch.multiprocessing as mp
 from os import listdir
 from os.path import isfile, join
 import numpy as np
+import pickle
 
 
 from util import *
@@ -204,6 +205,7 @@ def actor_self(actor_num, center_model, data_queue, signal_queue, summary_queue,
 
     n_epi = 0
     rollout = []
+    cache_bkup = []
     while True: # episode loop
         opp_model_num, opp_model_path = select_opponent(arg_dict)
         checkpoint = torch.load(opp_model_path, map_location=cpu_device)
@@ -274,6 +276,12 @@ def actor_self(actor_num, center_model, data_queue, signal_queue, summary_queue,
                 print(state_dict)
                 print("opp_obs np")
                 print(opp_state_dict)        
+                # dump rollout 
+                dump_dir = arg_dict['log_dir'] + '/dumps'
+                if not os.path.exists(dump_dir): 
+                    os.makedirs(dump_dir)
+                with open(f"{dump_dir}/actor_{actor_num}.dump", 'wb') as f_bkup:
+                    pickle.dump(cache_bkup, f_bkup)
 
             
             real_action, a, m, need_m, prob, prob_selected_a, prob_selected_m = get_action(a_prob, m_prob)
@@ -298,6 +306,9 @@ def actor_self(actor_num, center_model, data_queue, signal_queue, summary_queue,
                 data_queue.put(rollout)
                 rollout = []
                 model.load_state_dict(center_model.state_dict())
+            cache_bkup.append(transition)
+            if len(cache_bkup) > arg_dict["rollout_len"]:
+                cache_bkup.pop(0)
 
             steps += 1
             score += rew
